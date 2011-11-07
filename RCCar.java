cc315fb6c690f0244b21c4ba8c9ce97dd90d8050
Douwe Maan <douwemaan@me.com>
2011-11-07 15:43:26 +0100

    Allow for quick restarting of the connect-n(receive-handle)-disconnect cycle.

--- RCCar.java
+++ RCCar.java
@@ -8,38 +8,45 @@ import lejos.robotics.navigation.*;
 
 public class RCCar {
 	public static void main(String[] args) throws IOException {
-		System.out.println("Waiting for connection...");
-		
-		BTConnection connection = Bluetooth.waitForConnection();
-		if (connection == null) {
-			System.out.println("Failed to get connection");
-			return;
-		}
-
-		String friendlyDeviceName = RemoteDevice.getRemoteDevice(connection).getFriendlyName(false);
-		
-		System.out.println("Connected to " + friendlyDeviceName);
-		
-		DifferentialPilot pilot = new DifferentialPilot(2.9, 13.7, Motor.A, Motor.B, false);
-		RCCarEngine carEngine = new RCCarEngine(pilot, connection);
-		
-		System.out.println("Reading commands in loop");
-						
 		while (true) {
-			if (Button.ESCAPE.isPressed()) {
-				break;
+			System.out.println("Waiting for connection...");
+			
+			BTConnection connection = Bluetooth.waitForConnection();
+			if (connection == null) {
+				System.out.println("Failed to get connection");
+				return;
+			}
+	
+			String friendlyDeviceName = RemoteDevice.getRemoteDevice(connection).getFriendlyName(false);
+			
+			System.out.println("Connected to " + friendlyDeviceName);
+			
+			DifferentialPilot pilot = new DifferentialPilot(2.9, 13.7, Motor.A, Motor.B, false);
+			RCCarEngine carEngine = new RCCarEngine(pilot, connection);
+			
+			System.out.println("Reading commands in loop");
+							
+			while (true) {
+				if (Button.ESCAPE.isPressed()) {
+					break;
+				}
+				
+				boolean success = carEngine.waitForAndHandleCommand();
+				if (!success) {
+					break;
+				}
 			}
 			
-			boolean success = carEngine.waitForAndHandleCommand();
-			if (!success) {
+			System.out.println("Finished reading commands. Shutting down.");
+			
+			carEngine.close();
+			connection.close();
+			
+			System.out.println("Another round?");
+			
+			if (Button.waitForPress() == Button.ID_ESCAPE) {
 				break;
 			}
 		}
-		
-		System.out.println("Finished reading commands. Shutting down.");
-		
-		carEngine.close();
-		
-		Button.waitForPress();
 	}
 }
