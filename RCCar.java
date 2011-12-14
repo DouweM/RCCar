30a4346fada52489a74a8579a717a3eb14028402
Douwe Maan <douwemaan@me.com>
2011-12-14 17:13:04 +0100

    Use spaces instead of tabs for indentation.
    Add "?w=1" to the GitHub commit or diff URL, to have the diff ignore these whitespace changes.

--- RCCar.java
+++ RCCar.java
@@ -5,40 +5,40 @@ import lejos.nxt.comm.*;
 import lejos.robotics.navigation.*;
 
 public class RCCar {
-	public static void main(String[] args) throws IOException {
-		while (true) {
-			System.out.println("Waiting...");
-			
-			BTConnection connection = Bluetooth.waitForConnection(0, NXTConnection.RAW);
-			if (connection == null) {
-				System.out.println("Failed to get connection");
-				return;
-			}
-				
-			System.out.println("Connected");
-			
-			DifferentialPilot pilot = new DifferentialPilot(2.9, 13.7, Motor.A, Motor.B, false);
-			RCCarEngine carEngine = new RCCarEngine(pilot, connection);
-			
-			System.out.println("Reading commands");
-							
-			while (true) {				
-				boolean success = carEngine.waitForAndHandleCommand();
-				if (!success) {
-					break;
-				}
-			}
-			
-			System.out.println("Finished reading");
-			
-			carEngine.close();
-			connection.close();
-			
-			System.out.println("Another round?");
-			
-			if (Button.waitForPress() == Button.ID_ESCAPE) {
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("Waiting...");
+
+            BTConnection connection = Bluetooth.waitForConnection(0, NXTConnection.RAW);
+            if (connection == null) {
+                System.out.println("Failed to get connection");
+                return;
+            }
+
+            System.out.println("Connected");
+
+            DifferentialPilot pilot = new DifferentialPilot(2.9, 13.7, Motor.A, Motor.B, false);
+            RCCarEngine carEngine = new RCCarEngine(pilot, connection);
+
+            System.out.println("Reading commands");
+
+            while (true) {
+                boolean success = carEngine.waitForAndHandleCommand();
+                if (!success) {
+                    break;
+                }
+            }
+
+            System.out.println("Finished reading");
+
+            carEngine.close();
+            connection.close();
+
+            System.out.println("Another round?");
+
+            if (Button.waitForPress() == Button.ID_ESCAPE) {
+                break;
+            }
+        }
+    }
 }
--- RCCarEngine.java
+++ RCCarEngine.java
@@ -4,196 +4,196 @@ import lejos.robotics.navigation.*;
 import lejos.nxt.comm.*;
 
 public class RCCarEngine {
-	public static final byte COMMAND_TRAVEL		= 1 << 0; // Followed by speed: 	positive = forward, negative = backward
-	public static final byte COMMAND_STEER		= 1 << 1; // Followed by turnRate:	positive = right, 	negative = left
-	public static final byte COMMAND_STOP		= 1 << 2;
-	
-	public static enum TravelDirection { FORWARD, BACKWARD };
-	
-	private final DifferentialPilot _pilot;
-	private final DataInputStream _dataInStream;
+    public static final byte COMMAND_TRAVEL    = 1 << 0; // Followed by speed:     positive = forward, negative = backward
+    public static final byte COMMAND_STEER     = 1 << 1; // Followed by turnRate:  positive = right,   negative = left
+    public static final byte COMMAND_STOP      = 1 << 2;
+    
+    public static enum TravelDirection { FORWARD, BACKWARD };
+    
+    private final DifferentialPilot _pilot;
+    private final DataInputStream _dataInStream;
 
-	
-	private boolean _moving						= false;
-	private TravelDirection _travelDirection 	= TravelDirection.FORWARD;
-	private float _travelSpeed	 				= 0.0f;
-	private float _steerTurnRate 				= 0.0f;
-	
-	public RCCarEngine(DifferentialPilot pilot, NXTConnection connection) {
-		_pilot = pilot;
-		_dataInStream = connection.openDataInputStream();
-	}
-	
-	public void log(String message) {
-		System.out.println("E: " + message);
-	}
-	
-	public void close() {
-		try {
-			_moving = false;
-			_travelDirection = TravelDirection.FORWARD;
-			_travelSpeed = 0.0f;
-			_steerTurnRate = 0.0f;
-			
-			_pilot.stop();
-			
-			_dataInStream.close();
-			
-			try {
-				Thread.sleep(100); // Wait for data to drain
-			} catch (InterruptedException e) {}
-		} 
-		catch (IOException e) {
-			this.log("Failed to close: " + e);
-			return;
-		}
-	}
-	
-	public boolean waitForAndHandleCommand() {
-//		this.log("Waiting...");
-		
-		byte command;
-		try {
-			command = _dataInStream.readByte();
-		} 
-		catch (EOFException e) {
-			this.log("EOF reached.");
-			return false;
-		} 
-		catch (IOException e) {
-			this.log("Failed to read command: " + e);
-			return false;
-		}
-		
-//		this.log("Received " + command);
-		
-		return handleCommand(command);
-	}
-	
-	public boolean handleCommand(byte command) {
-		switch (command) {
-			case COMMAND_TRAVEL: {				
-				float speed;
-				try {
-					speed = _dataInStream.readFloat();
-				} 
-				catch (IOException e) {
-					this.log("Failed to read speed: " + e);
-					return false;
-				}
-								
-				this.doTravel(speed);
-				
-				break;
-			}
-			
-			case COMMAND_STEER: {				
-				float turnRate;
-				try {
-					turnRate = _dataInStream.readFloat();
-				} 
-				catch (IOException e) {
-					this.log("Failed to read turnRate: " + e);
-					return false;
-				}
-								
-				this.doSteer(turnRate);
-				
-				break;
-			}
-			
-			case COMMAND_STOP: {
-				this.doStop();
-				
-				break;
-			}
-			
-			default: {
-				this.log("Unrecognized command: " + command);
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	private float getLejosSteerTurnRate() {
-		float turnRate = _steerTurnRate;
-		
-		// direction: -1 = left, 1 = right
-		int direction = (int) Math.signum(turnRate);
-		
-		// Absolute turnRate should be kept between 0.0 and 100.0
-		turnRate = Math.abs(turnRate);
-		turnRate = (float) Math.max(0.0f, (float) Math.min(100.0f, turnRate));
-		
-		// Our turnRate: positive => right
-		// leJOS turnRate: negative => right
-		turnRate = -1 * direction * turnRate;
-		
-		// turnRate: ratio of inside to outside motor: ratio = 1 - turnRate / 100
-		// positive turnRate => left  motor drives the inside wheel => car turns left, forward  or right, backward
-		// negative turnRate => right                                            right, forward or left, backward
-		// turnRate == 0   => ratio = 1 - 0/100   = 1.0  => car travels in straight line
-		// turnRate == 100 => ratio = 1 - 100/100 = 0.0  => inside motor stops
-		// turnRate == 200 => ratio = 1 - 200/100 = -1.0 => car turns in place
-		
-		return turnRate;
-	}
-	
-	private void go() {
-		if (!_moving) {
-			return;
-		}
-		
-		// If turnRate == 0.0f, we're just traveling in a straight line.
-		float turnRate = this.getLejosSteerTurnRate();
-		
-		if (_travelDirection == TravelDirection.FORWARD) {
-			_pilot.steer(turnRate);
-		}
-		else {
-			_pilot.steerBackward(turnRate);
-		}
-	}
-	
-	private void doTravel(float speed) {
-		System.out.println("TRAVEL: " + speed);
+    
+    private boolean _moving                    = false;
+    private TravelDirection _travelDirection   = TravelDirection.FORWARD;
+    private float _travelSpeed                 = 0.0f;
+    private float _steerTurnRate               = 0.0f;
+    
+    public RCCarEngine(DifferentialPilot pilot, NXTConnection connection) {
+        _pilot = pilot;
+        _dataInStream = connection.openDataInputStream();
+    }
+    
+    public void log(String message) {
+        System.out.println("E: " + message);
+    }
+    
+    public void close() {
+        try {
+            _moving = false;
+            _travelDirection = TravelDirection.FORWARD;
+            _travelSpeed = 0.0f;
+            _steerTurnRate = 0.0f;
+            
+            _pilot.stop();
+            
+            _dataInStream.close();
+            
+            try {
+                Thread.sleep(100); // Wait for data to drain
+            } catch (InterruptedException e) {}
+        } 
+        catch (IOException e) {
+            this.log("Failed to close: " + e);
+            return;
+        }
+    }
+    
+    public boolean waitForAndHandleCommand() {
+//        this.log("Waiting...");
+        
+        byte command;
+        try {
+            command = _dataInStream.readByte();
+        } 
+        catch (EOFException e) {
+            this.log("EOF reached.");
+            return false;
+        } 
+        catch (IOException e) {
+            this.log("Failed to read command: " + e);
+            return false;
+        }
+        
+//        this.log("Received " + command);
+        
+        return handleCommand(command);
+    }
+    
+    public boolean handleCommand(byte command) {
+        switch (command) {
+            case COMMAND_TRAVEL: {                
+                float speed;
+                try {
+                    speed = _dataInStream.readFloat();
+                } 
+                catch (IOException e) {
+                    this.log("Failed to read speed: " + e);
+                    return false;
+                }
+                                
+                this.doTravel(speed);
+                
+                break;
+            }
+            
+            case COMMAND_STEER: {                
+                float turnRate;
+                try {
+                    turnRate = _dataInStream.readFloat();
+                } 
+                catch (IOException e) {
+                    this.log("Failed to read turnRate: " + e);
+                    return false;
+                }
+                                
+                this.doSteer(turnRate);
+                
+                break;
+            }
+            
+            case COMMAND_STOP: {
+                this.doStop();
+                
+                break;
+            }
+            
+            default: {
+                this.log("Unrecognized command: " + command);
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private float getLejosSteerTurnRate() {
+        float turnRate = _steerTurnRate;
+        
+        // direction: -1 = left, 1 = right
+        int direction = (int) Math.signum(turnRate);
+        
+        // Absolute turnRate should be kept between 0.0 and 100.0
+        turnRate = Math.abs(turnRate);
+        turnRate = (float) Math.max(0.0f, (float) Math.min(100.0f, turnRate));
+        
+        // Our turnRate: positive => right
+        // leJOS turnRate: negative => right
+        turnRate = -1 * direction * turnRate;
+        
+        // turnRate: ratio of inside to outside motor: ratio = 1 - turnRate / 100
+        // positive turnRate => left  motor drives the inside wheel => car turns left, forward  or right, backward
+        // negative turnRate => right                                            right, forward or left, backward
+        // turnRate == 0   => ratio = 1 - 0/100   = 1.0  => car travels in straight line
+        // turnRate == 100 => ratio = 1 - 100/100 = 0.0  => inside motor stops
+        // turnRate == 200 => ratio = 1 - 200/100 = -1.0 => car turns in place
+        
+        return turnRate;
+    }
+    
+    private void go() {
+        if (!_moving) {
+            return;
+        }
+        
+        // If turnRate == 0.0f, we're just traveling in a straight line.
+        float turnRate = this.getLejosSteerTurnRate();
+        
+        if (_travelDirection == TravelDirection.FORWARD) {
+            _pilot.steer(turnRate);
+        }
+        else {
+            _pilot.steerBackward(turnRate);
+        }
+    }
+    
+    private void doTravel(float speed) {
+        System.out.println("TRAVEL: " + speed);
 
-		_moving = true;
-		_travelSpeed = speed;
-		
-		if (speed < 0) {
-			_travelDirection = TravelDirection.BACKWARD;
-		}
-		else {
-			_travelDirection = TravelDirection.FORWARD;
-		}
-		speed = Math.abs(speed);
-		
-		_pilot.setTravelSpeed(speed);
-		
-		this.go();
-	}
-	
-	private void doSteer(float turnRate) {
-		System.out.println("STEER: " + turnRate);
-		
-		_steerTurnRate = turnRate;
-		
-		this.go();
-	}
-	
-	private void doStop() {
-		System.out.println("STOP");
-		
-		_moving = false;
-		_travelDirection = TravelDirection.FORWARD;
-		_travelSpeed = 0.0f;
-		_steerTurnRate = 0.0f;
-		
-		_pilot.stop();
-		_pilot.setTravelSpeed(0.0);
-		_pilot.steer(0);
-	}
+        _moving = true;
+        _travelSpeed = speed;
+        
+        if (speed < 0) {
+            _travelDirection = TravelDirection.BACKWARD;
+        }
+        else {
+            _travelDirection = TravelDirection.FORWARD;
+        }
+        speed = Math.abs(speed);
+        
+        _pilot.setTravelSpeed(speed);
+        
+        this.go();
+    }
+    
+    private void doSteer(float turnRate) {
+        System.out.println("STEER: " + turnRate);
+        
+        _steerTurnRate = turnRate;
+        
+        this.go();
+    }
+    
+    private void doStop() {
+        System.out.println("STOP");
+        
+        _moving = false;
+        _travelDirection = TravelDirection.FORWARD;
+        _travelSpeed = 0.0f;
+        _steerTurnRate = 0.0f;
+        
+        _pilot.stop();
+        _pilot.setTravelSpeed(0.0);
+        _pilot.steer(0);
+    }
 }
